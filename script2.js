d6e7f1d62ca8eb85242b74f97f65de41c9c67203
akanimoh <oakanimoh@gmail.com>
2023-01-31 15:15:23 +0100

    Made copy for clipboard functional and other adjestments to finish the project

--- script2.js
+++ script2.js
@@ -5,6 +5,7 @@ const lowerEl = document.getElementById('lower');
 const numberEl = document.getElementById('number');
 const symbolEl = document.getElementById('symbol');
 const btnEl = document.getElementById('generate');
+const copyEl = document.getElementById('copy');
 
 let upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
@@ -43,9 +44,24 @@ function getSymbols(params) {
      
      let password = '';
 
+     if (upperEl.checked) {
+        password += getUpper();
+   }
+
+   if (lowerEl.checked) {
+      password += getLower(); 
+     }
+
+  if (numberEl.checked) {
+      password += getNumbers();
+  }
+
+  if (symbolEl.checked) {
+      password += getSymbols();
+  }
      const len = lengthEl.value;
 
-     for (let i = 0; i < len; i++) {
+     for (let i = password.length; i < len; i++) {
          password += generateX()
      }
 
@@ -76,3 +92,27 @@ function getSymbols(params) {
  }
 
  btnEl.addEventListener('click', generatePw);
+
+ copyEl.addEventListener('click', ()=>{
+     /**
+ * The trick of copying to clipboard follows this process
+ * first, create a textarea
+ * second put the value you want to copy into the textarea
+ * third, append it to the DOM, bc execCommand copies from the DOM
+ * fourth, select content of the textarea
+ * fifth, use execCommand to copy
+ * sixth, remove textarea
+ */
+     const textarea = document.createElement('textarea');
+     const password = pwEl.innerText;
+
+    if(!password){return};
+
+     textarea.value = password;
+
+     document.body.appendChild(textarea);
+     textarea.select();
+     document.execCommand('copy');
+     textarea.remove();
+     alert("Password copied to clipboard");
+ })
